08cc942b00f68c4ad28d86fc2ae64cc4d106e7bf
daniel <daanielsharon@gmail.com>
2023-07-20 10:57:40 +0700

    testing user controller done

--- server/test/user_controller_test.go
+++ server/test/user_controller_test.go
@@ -2,15 +2,49 @@ package test
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"server/test/setup"
 	"strings"
+	"sync"
 	"testing"
 
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 )
+
+func Register(request string, wg *sync.WaitGroup, router *gin.Engine) {
+	recorder := httptest.NewRecorder()
+	requestBody := strings.NewReader(fmt.Sprintf(`{"username":"%v"}`, request))
+	go func() {
+		defer wg.Done()
+		request := httptest.NewRequest(http.MethodPost, "http://localhost:8080/api/v1/users/register", requestBody)
+		router.ServeHTTP(recorder, request)
+	}()
+}
+
+func Login(request string, wg *sync.WaitGroup, router *gin.Engine) (string, error) {
+	recorder := httptest.NewRecorder()
+	requestBody := strings.NewReader(fmt.Sprintf(`{"username":"%v"}`, request))
+	go func() {
+		defer wg.Done()
+		request := httptest.NewRequest(http.MethodPost, "http://localhost:8080/api/v1/users/register", requestBody)
+		router.ServeHTTP(recorder, request)
+	}()
+
+	response := recorder.Result()
+	headers := response.Header
+	cookie := headers.Get("Set-Cookie")
+
+	if cookie != "" {
+		return strings.Split(cookie, "=")[1], nil
+	}
+
+	err := fmt.Errorf("No cookie obtaind")
+	return "", err
+}
 
 func TestRegisterSuccess(t *testing.T) {
 	router, db := setup.All()
@@ -37,6 +71,125 @@ func TestRegisterSuccess(t *testing.T) {
 	assert.Equal(t, 200, int(responseBody["code"].(float64)))
 	assert.Equal(t, "OK", responseBody["status"].(string))
 	assert.Equal(t, "x", responseBody["data"].(map[string]interface{})["username"])
+}
+
+func TestRegisterFailBadRequest(t *testing.T) {
+	router, db := setup.All()
+	defer db.Close()
+
+	requestBody := strings.NewReader(`{"username":""}`)
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8080/api/v1/users/register", requestBody)
+	request.Header.Add("Content-Type", "application/json")
+
+	recorder := httptest.NewRecorder()
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	assert.Equal(t, 400, response.StatusCode)
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		t.Fail()
+	}
+
+	var responseBody map[string]interface{}
+	json.Unmarshal(body, &responseBody)
+
+	assert.Equal(t, 400, int(responseBody["code"].(float64)))
+	assert.Equal(t, "Bad Request", responseBody["status"].(string))
+}
+
+func TestRegisterFailInternalServerError(t *testing.T) {
+	router, db := setup.All()
+	defer db.Close()
+
+	requestBody := strings.NewReader(``)
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8080/api/v1/users/register", requestBody)
+	request.Header.Add("Content-Type", "application/json")
+
+	recorder := httptest.NewRecorder()
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	assert.Equal(t, 500, response.StatusCode)
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		t.Fail()
+	}
+
+	var responseBody map[string]interface{}
+	json.Unmarshal(body, &responseBody)
+
+	assert.Equal(t, 500, int(responseBody["code"].(float64)))
+	assert.Equal(t, "Internal Server Error", responseBody["status"].(string))
+}
+
+func TestLoginSuccess(t *testing.T) {
+	router, db := setup.All()
+	defer db.Close()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	Register("x", &wg, router)
+	wg.Wait()
+
+	requestBody := strings.NewReader(`{"username":"x"}`)
+	recorder := httptest.NewRecorder()
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8080/api/v1/users/login", requestBody)
+	request.Header.Add("Content-Type", "application/json")
+
+	router.ServeHTTP(recorder, request)
+	response := recorder.Result()
+
+	assert.Equal(t, 200, response.StatusCode)
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		t.Fail()
+	}
+
+	var responseBody map[string]interface{}
+	json.Unmarshal(body, &responseBody)
+
+	assert.Equal(t, 200, int(responseBody["code"].(float64)))
+	assert.Equal(t, "OK", responseBody["status"])
+
+	headers := response.Header
+	cookie := headers.Get("Set-Cookie")
+
+	assert.Contains(t, cookie, "token")
+}
+
+func TestLoginFailBadRequest(t *testing.T) {
+	router, db := setup.All()
+	defer db.Close()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	Register("x", &wg, router)
+	wg.Wait()
+
+	requestBody := strings.NewReader(`{"username":""}`)
+	recorder := httptest.NewRecorder()
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8080/api/v1/users/login", requestBody)
+	request.Header.Add("Content-Type", "application/json")
+
+	router.ServeHTTP(recorder, request)
+	response := recorder.Result()
+
+	assert.Equal(t, 400, response.StatusCode)
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		t.Fail()
+	}
+
+	var responseBody map[string]interface{}
+	json.Unmarshal(body, &responseBody)
+
+	assert.Equal(t, 400, int(responseBody["code"].(float64)))
+	assert.Equal(t, "Bad Request", responseBody["status"])
 
 	headers := response.Header
 	cookie := headers.Get("Set-Cookie")
@@ -44,4 +197,38 @@ func TestRegisterSuccess(t *testing.T) {
 	assert.NotContains(t, cookie, "token")
 }
 
-// func TestRegisterFail(t *testing.T)
+func TestLoginFailInternalServerError(t *testing.T) {
+	router, db := setup.All()
+	defer db.Close()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	Register("x", &wg, router)
+	wg.Wait()
+
+	requestBody := strings.NewReader(``)
+	recorder := httptest.NewRecorder()
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8080/api/v1/users/login", requestBody)
+	request.Header.Add("Content-Type", "application/json")
+
+	router.ServeHTTP(recorder, request)
+	response := recorder.Result()
+
+	assert.Equal(t, 500, response.StatusCode)
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		t.Fail()
+	}
+
+	var responseBody map[string]interface{}
+	json.Unmarshal(body, &responseBody)
+
+	assert.Equal(t, 500, int(responseBody["code"].(float64)))
+	assert.Equal(t, "Internal Server Error", responseBody["status"])
+
+	headers := response.Header
+	cookie := headers.Get("Set-Cookie")
+
+	assert.NotContains(t, cookie, "token")
+}
