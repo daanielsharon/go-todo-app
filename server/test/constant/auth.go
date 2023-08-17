3bba6df5000460545c0e3fbef07b17b4c7aabf9d
daniel <daanielsharon@gmail.com>
2023-08-17 09:42:25 +0700

    test main on user done

--- server/test/constant/auth.go
+++ server/test/constant/auth.go
@@ -2,7 +2,9 @@ package constant_test
 
 import (
 	"fmt"
+	"math/rand"
 	"strings"
+	"time"
 )
 
 const (
@@ -13,4 +15,11 @@ const (
 func RequestBody() *strings.Reader {
 	var requestBody = strings.NewReader(fmt.Sprintf(`{"username":"%v", "password":"%v"}`, Username, Password))
 	return requestBody
+}
+
+func RandomRequestBody() (*strings.Reader, *string, *string) {
+	var username string = fmt.Sprintf(`%v%v`, time.Now().Nanosecond(), rand.Intn(1000))
+	var password string = fmt.Sprintf(`%v`, rand.Intn(1000))
+	var requestBody = strings.NewReader(fmt.Sprintf(`{"username":"%v", "password":"%v"}`, username, password))
+	return requestBody, &username, &password
 }
